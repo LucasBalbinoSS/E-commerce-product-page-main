c18c77aa574c5d958e587010fee1070c9979e4ea
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2023-02-27 09:13:45 -0300

    Feat: JS - Função slider()
    A função slider() possibilita o usuário clicar nas setas laterais e conseguir ver as demais fotos, e além disso, na resolução desktop, adiciona a funcionalidade de um carrossel de pequenas imagens.

--- src/js/index.js
+++ src/js/index.js
@@ -51,3 +51,141 @@ const menu = () => {
         })
     }
 }
+
+const slider = () => {
+    padrao('padrao')
+    padrao('modal')
+
+
+    function padrao(alvo) {
+        const $setaVoltar = document.querySelector(`.js-container-seta-voltar-${alvo}`)
+        const $setaAvancar = document.querySelector(`.js-container-seta-avancar-${alvo}`)
+        const $containerImg = document.querySelector(`.js-container-img-${alvo}`)
+
+        const $imagens = document.querySelectorAll(`.js-img-tenis-${alvo}`)
+        const $imagensCarrossel = document.querySelectorAll(`.js-img-tenis-carrossel-${alvo}`)
+
+        let contador = $containerImg.getAttribute('data-foto-ativa')
+
+
+        // logica
+        carrosselImagens()
+        displaySetasValidar()
+        modalFotoAlternado()
+
+        $setaAvancar.addEventListener('click', function() {
+            fotoAvancar()
+            imagemCarrosselPreencher()
+            displaySetasValidar()
+        })
+
+        $setaVoltar.addEventListener('click', function() {
+            fotoVoltar()
+            imagemCarrosselPreencher()
+            displaySetasValidar()
+        })
+
+
+        // funcoes
+        function carrosselImagens() {
+            $imagensCarrossel.forEach(function($imagemCarrossel) {
+                imagemCarrosselPreencher()
+    
+                $imagemCarrossel.addEventListener('click', function() {
+                    contador = $imagemCarrossel.getAttribute('data-foto-carrossel-ativa')
+                    $containerImg.setAttribute('data-foto-ativa', contador)
+    
+                    displaySetasValidar()
+                    imagemCarrosselTrocar()
+    
+                    imagemCarrosselClicadaValidar()
+                    imagemCarrosselAtivar($imagemCarrossel)
+                })
+            })
+    
+    
+        }
+    
+        function imagemCarrosselPreencher() {
+            $imagensCarrossel.forEach(function($imagemCarrossel) {
+    
+                if ($imagemCarrossel.dataset.fotoCarrosselAtiva == contador) {
+                    imagemCarrosselClicadaValidar()
+                    imagemCarrosselAtivar($imagemCarrossel)
+                }
+            })
+        }
+    
+        function imagemCarrosselTrocar() {
+            $containerImg.style.transform = `translateX(-${contador - 1}00%)`
+        }
+    
+        function imagemCarrosselLimpar($alvo) {
+            $alvo.classList.remove('clicada')
+        }
+    
+        function imagemCarrosselClicadaValidar() {
+            $imagensCarrossel.forEach(function($imagemCarrossel) {
+                imagemCarrosselLimpar($imagemCarrossel)
+            })
+        }
+    
+        function imagemCarrosselAtivar($alvo) {
+            $alvo.classList.add('clicada')
+        }
+    
+        function fotoAvancar() {
+            $containerImg.style.transform = `translateX(-${contador}00%)`
+    
+            contador ++
+            $containerImg.setAttribute('data-foto-ativa', contador)
+        }
+    
+        function fotoVoltar() {
+            contador --
+            $containerImg.setAttribute('data-foto-ativa', contador)
+    
+            $containerImg.style.transform = `translateX(-${contador - 1}00%)`
+        }
+    
+        function displaySetasValidar() {
+    
+            if (contador == 1) {
+                esconder($setaVoltar)
+                mostrar($setaAvancar)
+            }
+    
+            if (contador > 1) {
+                mostrar($setaAvancar)
+                mostrar($setaVoltar)
+            }
+    
+            if (contador == $imagens.length) {esconder($setaAvancar)}
+        }
+
+        function modalFotoAlternado() {
+            const $modalFotoAlternado = document.querySelector('.modal-foto-alternado')
+            const $containerImgModal = document.querySelector('.js-container-img-modal')
+            const mobile = innerWidth < 1000
+
+            
+            $containerImg.addEventListener('click', function() {
+                $containerImgModal.setAttribute('data-foto-ativa', contador)
+                console.log(contador)
+    
+                if (mobile) {
+                    return
+                }
+
+                modalAbrir()
+            })
+        
+        
+            // funcoes
+            function modalAbrir() {
+                $modalFotoAlternado.classList.remove('esconder')
+                $modalFotoAlternado.style.display = 'flex'
+            }
+        }
+    }
+}
